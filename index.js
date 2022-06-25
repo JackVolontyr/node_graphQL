9a37675b5f03f030eb3222f6675e57224cee6590
Yevhen Volontyr <soapforwork@gmail.com>
2022-06-25 16:11:10 +0300

    ~start-dev to dev scripts, +isEnvDev, ~db (pm2)

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 const express = require("express");
 var { graphqlHTTP } = require('express-graphql');
 const path = require("path");
 
 const schema = require("./graphql/schema");
 const resolver = require("./graphql/resolver");
+const { isEnvDev } = require("./utils");
 const sequelize = require("./utils/db");
 
 const app = express();
@@ -29,8 +32,16 @@ async function start() {
 
 		sequelize.sync();
 
-		const PORT = process.env.PORT || 8002;
-		app.listen(PORT, _ => console.log(`Starts on ${PORT} PORT.`))
+		const PORT = process.env.PORT || (isEnvDev() ? 8001 : 8002); // :8002 for pm2
+		
+		app.listen(PORT, _ => {
+			if (isEnvDev()) {
+				console.log(`Starts on http://localhost:${PORT}/`);
+
+			} else {
+				console.log(`Starts on ${PORT} PORT.`);
+			}
+		});
 
 	} catch (error) { console.log(error); }
 }
--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,27 @@
 const { Sequelize } = require("sequelize");
-const { DB_NAME, USER_NAME, PASSWORD } = require("../keys");
+const { isEnvDev } = require(".");
 
-const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
-	host: 'localhost',
-	dialect: 'mysql',
-});
+const SEQUELIZE_OPTIONS = { host: 'localhost', dialect: 'mysql' };
+let DB_NAME, USER_NAME, PASSWORD;
+
+if (isEnvDev()) {
+	const keys = require("../keys");
+	DB_NAME = keys.DB_NAME; 
+	USER_NAME = keys.USER_NAME; 
+	PASSWORD = keys.PASSWORD; 
+
+} else {
+	DB_NAME = process.env.DB_NAME; 
+	USER_NAME = process.env.USER_NAME; 
+	PASSWORD = process.env.PASSWORD;
+
+	// TODO: rm for PRODUCTION (for locale pm2)
+	const keys = require("../keys");
+	DB_NAME = keys.DB_NAME; 
+	USER_NAME = keys.USER_NAME; 
+	PASSWORD = keys.PASSWORD; 
+}
+
+const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, SEQUELIZE_OPTIONS);
 
 module.exports = sequelize;
